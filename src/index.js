821a18cca538d2f2104c0e0fc1af16326d6aa6ea
ValeriaArellano0011 <valeria.arellano0011@gmail.com>
2023-01-04 15:31:26 -0300

    Configuracion de DB

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const server = require('./app');
+const { sequelize } = require("./db.js");
+
+async function main() {
+    try {
+      await sequelize.sync({force: true});
+      console.log("succesfully connected");
+      server.listen(3001);
+      console.log("server listening on port 3001");
+    } catch (error) {
+      console.error("Unable to connect to database");
+    }
+}
+  
+main();
