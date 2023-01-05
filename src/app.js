bde68099fc7e847d012930bda9a54adb6d5038da
IgnacioMelgarejo <melgarejonacho@hotmail.com>
2023-01-05 02:00:06 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cookieSession = require('cookie-session');
 const passport = require('passport');
 const passportSetup = require('../passport')
 const server = express();
-
+//bUENAS NOCHES 
 const routes = require('./routes/index.js');
 
 server.use((req, res, next)=>{
