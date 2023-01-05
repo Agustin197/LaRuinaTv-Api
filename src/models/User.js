3cd2711da34b31cd825ff36e608fe0134870cd1d
ValeriaArellano0011 <valeria.arellano0011@gmail.com>
2023-01-05 11:48:43 -0300

    model User created

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require('../db.js');
+
+const User = sequelize.define('user', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = {User}
