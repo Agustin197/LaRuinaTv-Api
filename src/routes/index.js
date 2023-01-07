db2c33d722240f10c57cf30441fae5d42cda384e
GwerhBard <lalo@perronegro.cl>
2023-01-07 12:06:09 -0300

    Checked

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,7 @@ const router = require('express').Router();
 const user = require('./user.js');
 const posts = require('./posts');
 
-
 router.use('/user', user);
 router.use("/posts", posts)
 
-module.exports =  router 
+module.exports =  router
