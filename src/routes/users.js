ad0e46ba01b796526b92b2c43bbaa3dee7c2303d
ValeriaArellano0011 <valeria.arellano0011@gmail.com>
2023-01-07 22:33:53 -0300

    loggin with database

---
+++ src/routes/users.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const server = express.Router();
+
+const { createUser, loginUser } = require("../controllers/users.js");
+
+server.post("/signup", async (req, res) => {
+  try {
+    const response = await createUser(req, res);
+    return res.status(200).json({ msg: response });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+server.post("/login", async (req, res) => {
+  try {
+    const response = await loginUser(req, res);
+    return res.status(200).json({ msg: response });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+module.exports = server
