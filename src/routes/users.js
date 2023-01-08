cab3f80a58c7a2b2caf8a9ac06fb5464dffbce85
GwerhBard <lalo@perronegro.cl>
2023-01-07 23:33:56 -0300

    asd

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,7 +4,6 @@ const server = express.Router();
 const { createUser, loginUser } = require("../controllers/users.js");
 
 /* -------------Account------------- */
-
 server.post("/signup", async (req, res) => {
   try {
     const response = await createUser(req, res);
