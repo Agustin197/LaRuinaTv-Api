b292f60cf6996ad67afc37dc3fed1d9cb0c49c98
GwerhBard <lalo@perronegro.cl>
2023-01-08 20:43:04 -0300

    fvck!ng thrvsh

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,13 +1,22 @@
 const { DB_LARUINATV_PRODUCTS } = require("../misc/product.js");
 
-const express = require('express')
+const express = require('express');
+const { getProductById } = require("../controllers/product.js");
 const server = express.Router()
 
-//-------------------user---------------------
+//-------------------Products---------------------
 
 server.get('/all', (req, res) => {
     try {
         return res.status(200).send(DB_LARUINATV_PRODUCTS) //test
+    } catch (error) {
+        return res.status(400).send('error')
+    }
+})
+server.get('/:id', (req, res) => {
+    let {id} = req.params
+    try {
+        return res.status(200).send(getProductById(id)) //test
     } catch (error) {
         return res.status(400).send('error')
     }
