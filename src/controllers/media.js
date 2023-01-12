da92f839eb4011b777fbf4b02c2fe940a5bae74d
GwerhBard <lalo@perronegro.cl>
2023-01-12 07:55:12 -0300

    fvckin thrvsh!

--- src/controllers/media.js
+++ src/controllers/media.js
@@ -153,7 +153,7 @@ async function listPostImages() {
       const linkimg = objs.map(o => imgLinks(o.id))
       const prop = objs.map(o => o.appProperties)
       const {categories, info, connectionId, title, genre, artist } = prop[0]
-      return [{sliderImg: linkimg[0], categories, info, connectionId, title, genre, artist}]
+      return {sliderImg: linkimg[0], categories, info, connectionId, title, genre, artist}
     }
 
     function imgLinks(id) {
@@ -162,7 +162,6 @@ async function listPostImages() {
     }
     response.data.files.map(async (e) => {
       const result = await createForGenerateUrl(e);
-      console.log(result)
       return result
     });
 
