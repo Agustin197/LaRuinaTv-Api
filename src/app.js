00dacd9aa0b14acf32d3652bff8356939497b42e
Lalo Freak <lalo.gwerh@gmail.com>
2023-02-02 00:54:29 -0300

    thrash

--- src/app.js
+++ src/app.js
@@ -10,37 +10,38 @@ const routes = require('./routes/index.js');
 
 server.use(session({ secret: 'dsgasdgsagafdgfgfdg' }));
 
-// server.use((req, res, next)=>{
-//     console.log('request from: ', req.headers.origin)
-//     const corsList = [
-//         'http://localhost:3000',
-//         'http://localhost:3001',
-//         'http://localhost:3002',
-//         'https://laruinarecords.cl',
-//         'https://tv.laruinarecords.cl'
-//     ];
-//     if(corsList.includes(req.headers.origin)){   
-//         res.header('Access-Control-Allow-Origin', (req.headers.origin));
-//         res.header('Access-Control-Allow-Credentials', 'true');
-//         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept');
-//         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//         next();
-//         }
-//     if(req.headers['user-agent'].includes('insomnia')) {
-//         res.header('Access-Control-Allow-Origin', '*');
-//         res.header('Access-Control-Allow-Credentials', 'true');
-//         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept');
-//         res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-//         next();
-//     } //testing ruteos
-// });
-server.use(cors({ origin: '*' }));
-server.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
+server.use((req, res, next)=>{
+    console.log('request from: ', req.headers.origin)
+    const corsList = [
+        'http://localhost:3000',
+        'http://localhost:3001',
+        'http://localhost:3002',
+        'https://laruinarecords.cl',
+        'https://tv.laruinarecords.cl',
+        'https://la-ruina-tv-client.vercel.app'
+    ];
+    if(corsList.includes(req.headers.origin)){   
+        res.header('Access-Control-Allow-Origin', (req.headers.origin));
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        next();
+        }
+    if(req.headers['user-agent'].includes('insomnia')) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+        next();
+    } //testing ruteos
 });
+// server.use(cors({ origin: '*' }));
+// server.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//     next();
+// });
 server.use(passport.initialize());
 //server.use(passport.session());
 
