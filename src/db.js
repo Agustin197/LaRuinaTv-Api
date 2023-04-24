c13cb76539dd1d650ee395e0964e12e2d59d8031
Lalo Freak <lalofreak.dev@gmail.com>
2023-04-24 12:25:59 -0400

    thrash

--- src/db.js
+++ src/db.js
@@ -1,7 +1,6 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-   // 'postgres://postgres:123321@localhost:5432/laruina',
    "postgres://postgres:rGsgb5JFeBBwx9U@terminalkiller-db.internal:5432",
   {
     logging: false,
